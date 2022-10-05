7d77ebf9013ed5d003561f7bcdc21cdb6c637038
Yash <yash111ams@gmail.com>
2022-10-05 18:12:27 +0530

    input todo item funcionality added

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 // HTML Section
 
-let div = document.createElement("div")
-div.id = "container"
+let div = document.createElement("div");
+div.id = "container";
 div.innerHTML = `
 <h1 class="heading"> Todo APP </h1>
 
@@ -12,7 +12,12 @@ div.innerHTML = `
 <button class="plus"> + </button>
 
 <ul class="list">
-        
+
+<li>Lorem ipsum dolor sit.<img class="delete" src="./images/delete.svg" alt="" srcset="" ></li>
+<li>Lorem ipsum dolor sit.<img class="delete" src="./images/delete.svg" alt="" srcset="" ></li>
+<li>Lorem ipsum dolor sit.<img class="delete" src="./images/delete.svg" alt="" srcset="" ></li>
+<li>Lorem ipsum dolor sit.<img class="delete" src="./images/delete.svg" alt="" srcset="" ></li>
+
 </ul>
 </div>
 
@@ -22,5 +27,30 @@ div.innerHTML = `
     <span>You have <span class="pendingTaskCount">0</span> Pending Tasks</span>
     <button> Clear All </button>
 </div>
-`
-document.body.appendChild(div)
+`;
+document.body.appendChild(div);
+
+// JavaScript Section
+
+let inputTodo = document.querySelector(".input-todo");
+let plusBtn = document.querySelector(".plus");
+let todoList = document.querySelector(".list");
+let clearAll = document.querySelector(".outputResult button");
+let deleteBtn = document.querySelector(".delete");
+
+inputTodo.onkeyup = () => {
+  let activePointer = `
+    opacity: 1;
+    pointer-events: auto;
+    `;
+  let removePointer = `
+    opacity: 0.5;
+    pointer-events: none;
+    `;
+    
+  if (inputTodo.value.trim() != 0) {
+    plusBtn.style.cssText = activePointer;
+  } else {
+    plusBtn.style.cssText = removePointer;
+  }
+};
